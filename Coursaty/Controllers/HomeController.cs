c0871797c832bdc0d235fcc21cfb6639cc54c5ab
emadsaber <omda.sharkawy@gmail.com>
2017-02-20 23:31:54 +0200

    Add display names for Course class

--- Coursaty/Controllers/HomeController.cs
+++ Coursaty/Controllers/HomeController.cs
@@ -24,7 +24,7 @@ namespace Coursaty.Controllers
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            //ViewBag.Message = "Your contact page.";
 
             return View();
         }
---
+++ Coursaty/Models/Helpers/CourseHelper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Coursaty.Models
+{
+    [MetadataType(typeof(CourseHelper))]
+    public partial class Course
+    {
+
+    }
+    public class CourseHelper
+    {
+        [DisplayName("Course Title")]
+        public string title { get; set; }
+        [DisplayName("Course URL")]
+        [Url]
+        public string link { get; set; }
+        [DisplayName("Course Thumbnail")]
+        public string image { get; set; }
+        [Range(1,5)]
+        [DisplayName("Rate")]
+        public decimal rate { get; set; }
+        [DisplayName("Views")]
+        public decimal views { get; set; }
+        [DisplayName("Course Description")]
+        public string description { get; set; }
+        public int instructorId { get; set; }
+    }
+}
